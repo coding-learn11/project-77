01a785cc7e3c9f2a072fd162b04c7910d12035ea
coding-learn11 <82395877+coding-learn11@users.noreply.github.com>
2021-10-04 12:49:29 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var F_name = "Aishwarya";
+        var L_name = " Agarwal";
+        var Full_name = F_name.concat(L_name);
+        console.log(Full_name);
+
+        var a = "15";
+        var b = "3";
+
+        var ads = +a + +b;
+        console.log(ads);
+
+        var sub = a - b;
+        console.log(sub);
+
+        var mul = a * b;
+        console.log(mul);
+
+        var divi = a / b;
+        console.log(divi);
